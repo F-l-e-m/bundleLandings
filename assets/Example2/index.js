fb81d501d4b9fa4447a17a4f5a37dceedd00f72d
Flem <FlemCode@yandex.ru>
2020-05-08 15:22:04 +0300

    add example2 landing

---
+++ assets/Example2/index.js
@@ -0,0 +1,7 @@
+import '@/_common/styles/normalize.scss';
+import '@/_common/styles/global.scss';
+import './assets/styles/style.scss';
+import '@/_common/js/api.js';
+import './assets/js/script.js';
+
+console.log('Landing example loaded');
